59d7da13281c480239236b950fb469cd6e885a52
Krylov Sergey <47079612+krylovsergey1999@users.noreply.github.com>
2019-12-10 23:51:57 +0300

    Добавление Routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./navbar/navbar";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Profile from "./pages/profile";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Navbar/>
+                <div className="container pt-4">
+                    <Switch>
+                        <Route path="/" exact component={Home}/>
+                        <Route path="/about" component={About}/>
+                        <Route path="/profile/:name" component={Profile}/>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
