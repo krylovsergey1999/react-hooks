665318db9eee4c5488f75b1153294975a13ba455
Krylov Sergey <47079612+krylovsergey1999@users.noreply.github.com>
2019-12-28 13:29:09 +0300

    Отображение профиля 1) используем useEffect

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -7,6 +7,10 @@ import {CLEAR_USERS, GET_REPOS, GET_USER, SEARCH_USERS, SET_LOADING} from "../ty
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 
+const withCreds = url => {
+    return `${url}client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+};
+
 const GithubState = ({children}) => {
     const initialState = {
         user: {},
@@ -21,30 +25,38 @@ const GithubState = ({children}) => {
         setLoading();
 
         const response = await axios.get(
-            `https://api.github.com/search/users?q=${value}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+            withCreds(`https://api.github.com/search/users?q=${value}&`)
         );
 
         dispatch({
-           type: SEARCH_USERS,
-           payload: response.data.items
-        });
+            type: SEARCH_USERS,
+            payload: response.data.items
+        })
     };
 
     const getUser = async name => {
-        // запрос на сервер
         setLoading();
+
+        const response = await axios.get(
+            withCreds(`https://api.github.com/users/${name}?`)
+        );
+
         dispatch({
             type: GET_USER,
-            payload: {}
+            payload: response.data
         })
     };
 
     const getRepos = async name => {
-        // запрос на сервер
         setLoading();
+
+        const response = await axios.get(
+            withCreds(`https://api.github.com/users/${name}/repos?per_page=5&`)
+        );
+
         dispatch({
             type: GET_REPOS,
-            payload: []
+            payload: response.data
         })
     };
 
